382c7081c7ce66046e3b71cd6c1f160590f3d8ed
Dmytro Astakhov <astahov.dmitriy.a@gmail.com>
2025-07-26 15:26:15 +0300

    two pages of markup were made

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script setup>
+import TheWelcome from '@/components/pages/TheWelcome.vue'
+import Layout from '@/components/layouts/Layout.vue'
+import TheDashboard from '@/components/pages/TheDashboard.vue'
+import TheWorkout from '@/components/pages/TheWorkout.vue'
+import { ref } from 'vue'
 
-<template></template>
+const selectedDisplay = ref(3)
+</script>
+
+<template>
+  <Layout>
+    <!--PAGE 1-->
+    <TheWelcome v-if="selectedDisplay === 1" />
+    <!--PAGE 2-->
+    <TheDashboard v-if="selectedDisplay === 2" />
+    <!--PAGE 3-->
+    <TheWorkout v-if="selectedDisplay === 3" />
+  </Layout>
+</template>
 
 <style scoped></style>
